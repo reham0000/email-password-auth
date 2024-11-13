f747e611f838beace1df7d450f0714c3598f4d6c
reham0000 <amsarkerreham@gmail.com>
2024-11-13 14:09:23 +0600

    sign in form done

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,29 +1,51 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.init";
 import { useState } from "react";
+import { FaEye } from "react-icons/fa";
 
 const SignUp = () => {
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [errorMessage, setErrorMessage] = useState('')
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const terms = e.target.terms.checked;
+    console.log(email, password, terms);
 
-    const handleSignUp = (e) => {
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email, password);
+    setErrorMessage("");
+    setSuccess(false);
 
-        setErrorMessage('');
-
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((result) => {
-            console.log(result.user);
-        })
-        .catch ((error) => {
-            console.log('ERROR', error.message);
-            setErrorMessage(error.message)
-        })
+    if(!terms) {
+      setErrorMessage("Please accept the terms and conditions");
     }
 
+    if (password < 6) {
+      setErrorMessage("Password should be 6 characters or longer");
+      return;
+    }
+
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;"'<>?,./\\|-]).{6,}$/;
+
+    if (!regex.test(password)) {
+      setErrorMessage("Password is not valid.");
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccess(true);
+      })
+      .catch((error) => {
+        console.log("ERROR", error.message);
+        setErrorMessage(error.message);
+        setSuccess(false);
+      });
+  };
 
   return (
     <div className="my-8 card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
@@ -34,37 +56,49 @@ const SignUp = () => {
             <span className="label-text">Email</span>
           </label>
           <input
-          name="email"
+            name="email"
             type="email"
             placeholder="email"
             className="input input-bordered"
             required
           />
         </div>
-        <div className="form-control">
+        <div className="form-control relative">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
           <input
-          name="password"
-            type="password"
+            name="password"
+            type={showPassword ? "text" : "password"}
             placeholder="password"
-            className="input input-bordered"
+            className="input input-bordered  "
             required
           />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="right-4 top-12 absolute"
+          >
+            <FaEye />
+          </button>
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">
               Forgot password?
             </a>
           </label>
         </div>
+        <div className="form-control">
+          <label className="label justify-start cursor-pointer">
+          <input type="checkbox" name="terms" className="checkbox mr-4" />
+            <span className="label-text">Remember me</span>
+            
+          </label>
+        </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Sign Up</button>
         </div>
       </form>
-      {
-        errorMessage && <p className="text-red-500">{errorMessage}</p>
-      }
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      {success && <p className="text-green-600">Sign up Successful</p>}
     </div>
   );
 };
